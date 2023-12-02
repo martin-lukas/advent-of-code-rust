74dba39cfa5929b706d329c5156849681be10516
Martin Lukas <martin.lukas@email.com>
2023-12-02 10:23:31 +0000

    WIP day 3

---
+++ src/day3/mod.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+
+use std::collections::HashSet;
+use std::fs;
+
+use itertools::Itertools;
+
+pub fn solution() -> String {
+    let data = fs::read_to_string("src/day3/input.txt").unwrap();
+    let rucksacks = data.lines().collect::<Vec<&str>>();
+    let shared_item_priorities = rucksacks
+        .clone()
+        .into_iter()
+        .map(split_in_half)
+        .map(|(first, second)| (to_char_set(first), to_char_set(second)))
+        .map(|(first, second)| {
+            first
+                .intersection(&second)
+                .into_iter()
+                .map(char_score)
+                .sum::<u32>()
+        })
+        .sum::<u32>();
+    // let shared_item_among_3_priorities = rucksacks
+    //     .chunks(3)
+    //     .map(|[f, s, t, ..]| [to_char_set(*f), to_char_set(*s), to_char_set(*t)])
+    //     .map(|[f, s, t]| {
+    //         f.intersection(&s)
+    //             .collect::<HashSet<_>>()
+    //             .iter()
+    //             .map(|c| **c)
+    //             .collect::<HashSet<_>>()
+    //             .intersection(&t)
+    //             .into_iter()
+    //             .map(char_score)
+    //             .sum::<u32>()
+    //     })
+    //     .sum::<u32>();
+
+    format!(
+        "Priorities for shared item in rucksack: {}\n\
+        Priorities for shared item in 3 rucksacks: ",
+        shared_item_priorities,
+        // shared_item_among_3_priorities
+    )
+}
+
+fn split_in_half(str: &str) -> (&str, &str) {
+    str.split_at(str.len() / 2)
+}
+
+fn to_char_set(str: &str) -> HashSet<char> {
+    HashSet::from_iter(str.chars().into_iter())
+}
+
+fn char_score(c: &char) -> u32 {
+    if (c.is_ascii_uppercase()) {
+        *c as u32 - 38
+    } else {
+        *c as u32 - 96
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let _ = color_eyre::install();
 
     // println!("{}", day1::solution());
-    println!("{}", day2::solution());
+    // println!("{}", day2::solution());
+    println!("{}", day3::solution());
 }
